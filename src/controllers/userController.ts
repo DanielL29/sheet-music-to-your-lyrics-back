8477eca4ed82cfd368504fb160c1cad76693cfad
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-09-28 17:37:20 -0300

    feat: userController(login)

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import userService from '../services/userService';
-import { UserInsertData } from '../types/userType';
+import { UserInsertData, UserLogin } from '../types/userType';
 
 async function insert(req: Request, res: Response) {
   const user: UserInsertData = req.body;
@@ -10,8 +10,17 @@ async function insert(req: Request, res: Response) {
   res.sendStatus(201);
 }
 
+async function login(req: Request, res: Response) {
+  const user: UserLogin = req.body;
+
+  const token: string = await userService.login(user);
+
+  res.status(200).send({ token });
+}
+
 const userController = {
   insert,
+  login,
 };
 
 export default userController;
