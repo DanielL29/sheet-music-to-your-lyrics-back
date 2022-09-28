7ab2c113807bf164badd0fd5f1882d4e75ed4b21
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-09-28 18:46:22 -0300

    test: user tests unit POST /sing-in, expect to found email and receive a token, not found email and throw not found and receive a wrong password throw a bad request of wrong password

--- tests/unit/user.spec.ts
+++ tests/unit/user.spec.ts
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import errors from '../../src/errors/errorsThrow';
 import userFactory from '../factories/userFactory';
 import userRepository from '../../src/repositories/userRepository';
@@ -18,6 +19,7 @@ describe('POST /users/sign-up', () => {
     await expect(
       userService.insert({ ...user, confirmPassword: user.password }),
     ).resolves.not.toThrow();
+
     expect(userRepository.findByEmail).toBeCalledWith(user.email);
     expect(userRepository.insert).toBeCalled();
   });
@@ -36,7 +38,59 @@ describe('POST /users/sign-up', () => {
     await expect(
       userService.insert({ ...user, confirmPassword: user.password }),
     ).rejects.toEqual(errors.conflict('user', 'registered'));
+
     expect(userRepository.findByEmail).toBeCalledWith(user.email);
     expect(userRepository.insert).not.toBeCalled();
+  });
+});
+
+describe('POST /users/sign-in', () => {
+  it('expect to found a email and receive a token', async () => {
+    const user = userFactory.__createUser();
+    const token = userFactory.__genereateToken(1, user.name);
+
+    jest.spyOn(userRepository, 'findByEmail').mockResolvedValueOnce({
+      id: 1,
+      ...user,
+      createdAt: new Date(),
+    });
+    jest.spyOn(userService, 'generateValidToken').mockReturnValueOnce(token);
+    jest.spyOn(bcrypt, 'compareSync').mockReturnValueOnce(true);
+
+    const validToken = await userService.login({ email: user.email, password: user.password });
+
+    expect(validToken).toEqual(token);
+    expect(userRepository.findByEmail).toBeCalledWith(user.email);
+    expect(bcrypt.compareSync).toBeCalled();
+  });
+
+  it('expect to not found email and throw not found', async () => {
+    const user = userFactory.__createUser();
+
+    jest.spyOn(userRepository, 'findByEmail').mockResolvedValueOnce(null);
+
+    await expect(
+      userService.login({ email: user.email, password: user.password }),
+    ).rejects.toEqual(errors.notFound('user', 'users'));
+
+    expect(userRepository.findByEmail).toBeCalledWith(user.email);
+  });
+
+  it('expect to receive a wrong password and throw a bad request', async () => {
+    const user = userFactory.__createUser();
+
+    jest.spyOn(userRepository, 'findByEmail').mockResolvedValueOnce({
+      id: 1,
+      ...user,
+      createdAt: new Date(),
+    });
+    jest.spyOn(bcrypt, 'compareSync').mockReturnValueOnce(false);
+
+    await expect(
+      userService.login({ email: user.email, password: user.password }),
+    ).rejects.toEqual(errors.badRequest('Wrong password'));
+
+    expect(userRepository.findByEmail).toBeCalledWith(user.email);
+    expect(bcrypt.compareSync).toBeCalled();
   });
 });
