a3f43b8bdea34defa7a0858cbe3cbb3d69d6cdd8
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-09-28 17:34:47 -0300

    feat: userService(login), user type UserLogin

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,8 +1,12 @@
 import { User } from '@prisma/client';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
 import errors from '../errors/errorsThrow';
 import userRepository from '../repositories/userRepository';
 import { UserInsertData } from '../types/userType';
 import hash from '../utils/hashUtil';
+
+const secretKey = process.env.SECRET_KEY;
 
 async function insert(user: UserInsertData) {
   const isUser: User | null = await userRepository.findByEmail(user.email);
@@ -17,8 +21,21 @@ async function insert(user: UserInsertData) {
   await userRepository.insert({ ...user, password: encryptedPassword });
 }
 
+async function login(user: UserInsertData): Promise<string> {
+  const isUser: User | null = await userRepository.findByEmail(user.email);
+
+  if (!isUser) {
+    throw errors.notFound('user', 'users');
+  }
+
+  const token = jwt.sign({ id: isUser.id, name: isUser.name }, secretKey!, { expiresIn: '1h' });
+
+  return token;
+}
+
 const userService = {
   insert,
+  login,
 };
 
 export default userService;
--- src/types/userType.ts
+++ src/types/userType.ts
@@ -1,3 +1,4 @@
 import { User } from '@prisma/client';
 
 export type UserInsertData = Omit<User, 'id' | 'createdAt'> & { confirmPassword?: string }
+export type UserLogin = Omit<User, 'id' | 'name' | 'createdAt'>
