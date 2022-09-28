6f7055341f88fdcf0c23f0c2100f7bb0aaa328c0
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-09-28 15:36:49 -0300

    feat: userSchema

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -1,10 +1,12 @@
 import { ObjectSchema } from 'joi';
+import userSchema from './userSchema';
 
 export interface Schemas {
   [key: string]: ObjectSchema
 }
 
 const schemas: Schemas = {
+  user: userSchema,
 };
 
 export default schemas;
