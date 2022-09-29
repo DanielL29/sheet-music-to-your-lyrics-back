ef2123b610425305366f0557c813ab3986028e34
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-09-29 16:58:45 -0300

    feat: musicController(update)

--- src/services/musicService.ts
+++ src/services/musicService.ts
@@ -3,15 +3,16 @@ import dotenv from 'dotenv';
 import axios from 'axios';
 import errors from '../errors/errorsThrow';
 import musicRepository from '../repositories/musicRepository';
-import { MusicSchema } from '../types/musicType';
+import { MusicSchema, MusicUpdateData, MusicVagalumeData } from '../types/musicType';
 import categoryRepository from '../repositories/categoryRepository';
 import S3Storage from '../classes/S3Storage';
 
 dotenv.config();
 
 const { VAGALUME_API_URL, VAGALUME_API_KEY } = process.env;
+const s3Storage = new S3Storage();
 
-async function verifyMusicAndCategory(name: string, categoryId: number) {
+async function verifyMusicAndCategory(name: string, categoryId: number): Promise<void> {
   const isMusic: Music | null = await musicRepository.findByName(name);
 
   if (isMusic) {
@@ -25,7 +26,7 @@ async function verifyMusicAndCategory(name: string, categoryId: number) {
   }
 }
 
-async function getMusicFromVagalume(music: MusicSchema) {
+async function getMusicFromVagalume(music: MusicSchema): Promise<MusicVagalumeData> {
   let translatedLyric = null;
 
   const { data: musicFromVagalume } = await axios.get(VAGALUME_API_URL!, {
@@ -59,8 +60,6 @@ async function getMusicFromVagalume(music: MusicSchema) {
 async function insertFileInAWS(
   sheetMusicFile: Express.Multer.File | undefined,
 ): Promise<string | null> {
-  const s3Storage = new S3Storage();
-
   if (!sheetMusicFile) {
     return null;
   }
@@ -70,7 +69,10 @@ async function insertFileInAWS(
   return sheetMusicFile.filename;
 }
 
-async function insert(music: MusicSchema, sheetMusicFile: Express.Multer.File | undefined) {
+async function insert(
+  music: MusicSchema,
+  sheetMusicFile: Express.Multer.File | undefined,
+): Promise<void> {
   const {
     lyric, translatedLyric, name, author,
   } = await getMusicFromVagalume(music);
@@ -90,8 +92,54 @@ async function insert(music: MusicSchema, sheetMusicFile: Express.Multer.File | 
   });
 }
 
+function buildUpdateObject(
+  musicUpdate: MusicUpdateData,
+  sheetMusicFile: Express.Multer.File | undefined,
+): MusicUpdateData {
+  const musicUpdateObj: any = {};
+
+  if (musicUpdate.musicHelpVideoUrl !== '') {
+    musicUpdateObj.musicHelpVideoUrl = musicUpdate.musicHelpVideoUrl;
+  }
+
+  if (musicUpdate.musicVideoUrl !== '') {
+    musicUpdateObj.musicVideoUrl = musicUpdate.musicVideoUrl;
+  }
+
+  if (sheetMusicFile) {
+    musicUpdateObj.sheetMusicFile = sheetMusicFile.filename;
+  }
+
+  return musicUpdateObj;
+}
+
+async function update(
+  musicId: number,
+  music: MusicUpdateData,
+  sheetMusicFile: Express.Multer.File | undefined,
+): Promise<void> {
+  const isMusic = await musicRepository.findById(musicId);
+
+  if (!isMusic) {
+    throw errors.notFound('music', 'musics');
+  }
+
+  if (isMusic.sheetMusicFile && sheetMusicFile) {
+    await s3Storage.deleteFile(isMusic.sheetMusicFile);
+
+    await s3Storage.saveFile(sheetMusicFile.filename);
+  } else if (sheetMusicFile) {
+    await s3Storage.saveFile(sheetMusicFile.filename);
+  }
+
+  const musicUpdateObj = buildUpdateObject(music, sheetMusicFile);
+
+  await musicRepository.update(musicId, musicUpdateObj);
+}
+
 const musicService = {
   insert,
+  update,
 };
 
 export default musicService;
