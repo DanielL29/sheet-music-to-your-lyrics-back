6211c4a136f83ebdf9224b3d2fa6b82bc08395f1
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-09-28 22:43:11 -0300

    feat: musicRepository

---
+++ src/repositories/musicRepository.ts
@@ -0,0 +1,18 @@
+import { Music } from '@prisma/client';
+import prisma from '../database';
+import { MusicInsertData } from '../types/musicType';
+
+async function findByName(name: string): Promise<Music> {
+  return prisma.music.findUnique({ where: { name } });
+}
+
+async function insert(music: MusicInsertData) {
+  await prisma.music.create({ data: music });
+}
+
+const musicRepository = {
+  insert,
+  findByName,
+};
+
+export default musicRepository;
