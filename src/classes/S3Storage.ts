83bbe3af232874312e950f17f82bad53f868847a
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-09-29 02:32:13 -0300

    feat: add delete file from aws s3 bucket method in class s3storage

--- src/classes/S3Storage.ts
+++ src/classes/S3Storage.ts
@@ -35,6 +35,14 @@ class S3Storage {
 
     await fs.promises.unlink(originalPath);
   }
+
+  async deleteFile(filename: string): Promise<void> {
+    this.client.deleteObject({
+      Bucket: 'sheet-music-to-your-lyrics',
+      Key: filename,
+    })
+      .promise();
+  }
 }
 
 export default S3Storage;
