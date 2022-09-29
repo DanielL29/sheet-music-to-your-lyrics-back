6e999062e1a750761ef0bf4ff4a44404c770bed8
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-09-29 16:55:20 -0300

    feat: add types to musicTypes

--- src/types/musicType.ts
+++ src/types/musicType.ts
@@ -2,3 +2,10 @@ import { Music } from '@prisma/client';
 
 export type MusicInsertData = Omit<Music, 'id' | 'createdAt'>
 export type MusicSchema = Omit<MusicInsertData, 'lyric' | 'translatedLyric'>
+export type MusicUpdateData = Omit<MusicSchema, 'name' | 'author' | 'categoryId'>
+export interface MusicVagalumeData {
+  name: string
+  lyric: string
+  translatedLyric: string
+  author: string
+}
