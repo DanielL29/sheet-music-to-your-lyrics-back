8ac342c5fcf8df888fc31dcaac376522dc9c31e2
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-09-29 17:27:53 -0300

    feat: music route update

--- src/routes/musicRouter.ts
+++ src/routes/musicRouter.ts
@@ -2,12 +2,28 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from '../config/multer';
 import musicController from '../controllers/musicController';
+import validateEmptyMusicUpdate from '../middlewares/validateEmptyMusicUpdate';
 import validateSchemas from '../middlewares/validateSchemas';
 import validateToken from '../middlewares/validateToken';
 
 const musicRouter = Router();
 const upload = multer(multerConfig);
 
-musicRouter.post('/create', validateToken, upload.single('sheetMusicFile'), validateSchemas('music'), musicController.insert);
+musicRouter.post(
+  '/create',
+  validateToken,
+  upload.single('sheetMusicFile'),
+  validateSchemas('music'),
+  musicController.insert,
+);
+
+musicRouter.patch(
+  '/update/:musicId',
+  validateToken,
+  upload.single('sheetMusicFile'),
+  validateSchemas('musicUpdate'),
+  validateEmptyMusicUpdate,
+  musicController.update,
+);
 
 export default musicRouter;
