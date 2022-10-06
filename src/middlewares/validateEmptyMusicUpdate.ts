84e1e39fc0c8808f6b4608e9b1f1feba2093b476
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-10-05 23:11:41 -0300

    fix: condition on validateEmptyMusicUpdate

--- src/middlewares/validateEmptyMusicUpdate.ts
+++ src/middlewares/validateEmptyMusicUpdate.ts
@@ -2,12 +2,14 @@ import { NextFunction, Request, Response } from 'express';
 import errors from '../errors/errorsThrow';
 
 export default function validateEmptyMusicUpdate(req: Request, res: Response, next: NextFunction) {
-  const { musicVideoUrl, musicHelpVideoUrl } = req.body;
+  const { musicVideoUrl, musicHelpVideoUrl, lyric } = req.body;
   const sheetMusicFile = req.file;
 
-  if (sheetMusicFile || musicHelpVideoUrl || musicVideoUrl) {
+  if (sheetMusicFile || musicHelpVideoUrl || musicVideoUrl || lyric) {
     next();
   } else {
-    throw errors.unprocessableEntity('at least one of elements("musicVideoUrl", "musicHelpVideoUrl", "sheetMusicFile") cannot be empty');
+    throw errors.unprocessableEntity(
+      'at least one of elements("musicVideoUrl", "musicHelpVideoUrl", "sheetMusicFile", "lyric") cannot be empty',
+    );
   }
 }
