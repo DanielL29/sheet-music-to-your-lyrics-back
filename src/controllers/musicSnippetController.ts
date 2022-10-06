0a4d8fe4bef16fca362f9a4e55c4a157a49f4287
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-10-06 01:51:59 -0300

    feat: musicSnippet controller

---
+++ src/controllers/musicSnippetController.ts
@@ -0,0 +1,30 @@
+import { MusicSnippet } from '@prisma/client';
+import { Request, Response } from 'express';
+import musicSnippetService from '../services/musicSnippetService';
+import { MusicSnippetInsertData } from '../types/musicSnippetType';
+
+async function insert(req: Request, res: Response) {
+  const userId: number = res.locals.user.id;
+  const { musicName } = req.params;
+  const musicSnippet: MusicSnippetInsertData = req.body;
+  const snippetAidFile: Express.Multer.File | undefined = req.file;
+
+  await musicSnippetService.insert(userId, musicName, musicSnippet, snippetAidFile);
+
+  res.sendStatus(201);
+}
+
+async function findMusicSnippets(req: Request, res: Response) {
+  const { musicName } = req.params;
+
+  const musicSnippets: MusicSnippet[] = await musicSnippetService.findMusicSnippets(musicName);
+
+  res.status(200).send(musicSnippets);
+}
+
+const musicSnippetController = {
+  insert,
+  findMusicSnippets,
+};
+
+export default musicSnippetController;
