09f314f8ed4d6788318456bb6ef0d8dd94609934
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-10-07 03:48:49 -0300

    feat: find authors controller

--- src/controllers/authorController.ts
+++ src/controllers/authorController.ts
@@ -1,16 +1,24 @@
+import { Author } from '@prisma/client';
 import { Request, Response } from 'express';
 import authorService from '../services/authorService';
 
 async function getByCategory(req: Request, res: Response) {
   const { category } = req.params;
 
-  const authorsByCategory = await authorService.findByCategory(category);
+  const authorsByCategory = await authorService.findAuthorByCategory(category);
 
   res.status(200).send(authorsByCategory);
 }
 
+async function getAll(req: Request, res: Response) {
+  const authors: Author[] = await authorService.findAuthors();
+
+  res.status(200).send(authors);
+}
+
 const authorController = {
   getByCategory,
+  getAll,
 };
 
 export default authorController;
--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -2,8 +2,8 @@ import { Category } from '@prisma/client';
 import { Request, Response } from 'express';
 import categoryService from '../services/categoryService';
 
-async function getAll(req: Request, res: Response) {
-  const categories: Category[] = await categoryService.findAll();
+async function getAll(_: Request, res: Response) {
+  const categories: Category[] = await categoryService.findCategories();
 
   res.status(200).send(categories);
 }
