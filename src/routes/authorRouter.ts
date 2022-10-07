2fe0f26ff52f2e71fbf46ab9f62d4fb50666153a
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-10-07 03:49:04 -0300

    feat: find authors route

--- src/routes/authorRouter.ts
+++ src/routes/authorRouter.ts
@@ -5,5 +5,6 @@ import validateToken from '../middlewares/validateToken';
 const authorRouter = Router();
 
 authorRouter.get('/:category', validateToken, authorController.getByCategory);
+authorRouter.get('/', validateToken, authorController.getAll);
 
 export default authorRouter;
