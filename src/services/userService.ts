f85a7e7b24b2914580e1d8bb399823d3f012aeee
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-10-08 01:06:28 -0300

    fix: generate token & feat: send email, update teacher to contributor

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,14 +1,52 @@
 import { User } from '@prisma/client';
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
+import sgMail from '@sendgrid/mail';
 import errors from '../errors/errorsThrow';
 import userRepository from '../repositories/userRepository';
-import { UserInsertData, UserLogin } from '../types/userType';
+import { UserInsertData, UserLocal, UserLogin } from '../types/userType';
 import hash from '../utils/hashUtil';
 
 dotenv.config();
 
-const secretKey = process.env.SECRET_KEY;
+const { SECRET_KEY, SENDGRID_API_KEY, APP_URL } = process.env;
+
+async function sendEmailToContributor(email: string): Promise<void> {
+  sgMail.setApiKey(SENDGRID_API_KEY!);
+
+  const msg = {
+    to: email,
+    from: 'daniell.ederli@hotmail.com',
+    subject: 'Torne se um contribuidor!',
+    text: 'Para validar e se tornar contribuidor, clique no link abaixo: ',
+    html: `${APP_URL}/become/contributor?email=${email}`,
+  };
+
+  await sgMail.send(msg);
+}
+
+function generateValidToken(user: User): string {
+  return jwt.sign({
+    id: user.id, email: user.email, teacher: user.teacher,
+  }, SECRET_KEY!, { expiresIn: '1h' });
+}
+
+async function makeUserContributor(email: string): Promise<UserLocal> {
+  const isUser = await userRepository.findByEmail(email);
+
+  if (!isUser) {
+    throw errors.notFound('user', 'users');
+  }
+
+  await userRepository.updateMakeTeacher(email);
+
+  const token = generateValidToken(isUser);
+
+  return {
+    token,
+    teacher: true,
+  };
+}
 
 async function insert(user: UserInsertData) {
   const isUser: User | null = await userRepository.findByEmail(user.email);
@@ -20,14 +58,14 @@ async function insert(user: UserInsertData) {
   const encryptedPassword = hash.encrypt(user.password);
   delete user.confirmPassword;
 
-  await userRepository.insert({ ...user, password: encryptedPassword });
+  await userRepository.insert({ ...user, password: encryptedPassword, teacher: false });
+
+  if (user.teacher) {
+    await sendEmailToContributor(user.email);
+  }
 }
 
-function generateValidToken(id: number, name: string): string {
-  return jwt.sign({ id, name }, secretKey!, { expiresIn: '1h' });
-}
-
-async function login(user: UserLogin): Promise<string> {
+async function login(user: UserLogin): Promise<UserLocal> {
   const isUser: User | null = await userRepository.findByEmail(user.email);
 
   if (!isUser) {
@@ -38,15 +76,20 @@ async function login(user: UserLogin): Promise<string> {
     throw errors.badRequest('Wrong password');
   }
 
-  const token = generateValidToken(isUser.id, isUser.name);
+  const token = generateValidToken(isUser);
 
-  return token;
+  return {
+    token,
+    teacher: isUser.teacher,
+  };
 }
 
 const userService = {
   insert,
   login,
   generateValidToken,
+  makeUserContributor,
+  sendEmailToContributor,
 };
 
 export default userService;
