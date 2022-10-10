8558ad9dfd6364cde7fa05f9068c2956c907936f
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-10-10 16:01:24 -0300

    fix: user login type

--- src/types/userType.ts
+++ src/types/userType.ts
@@ -1,7 +1,7 @@
 import { User } from '@prisma/client';
 
 export type UserInsertData = Omit<User, 'id' | 'createdAt'> & { confirmPassword?: string }
-export type UserLogin = Omit<User, 'id' | 'name' | 'createdAt'>
+export type UserLogin = Omit<User, 'id' | 'name' | 'createdAt' | 'teacher'>
 
 export interface UserLocal {
   token: string
